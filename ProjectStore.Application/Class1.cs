1bf0bf1c36e2a82a5c54dee14de67953123d0210
artemov <artik3314@gmail.com>
2024-08-01 10:05:05 +0300

    Настройка gitignore

---
+++ ProjectStore.Application/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ProjectStore.Application;
+
+public class Class1
+{
+}
---
+++ ProjectStore.Domen/Validations/RepositoryValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectStore.Domen.Validations;
+
+public class RepositoryValidator : AbstractValidator<Repository>
+{
+    public RepositoryValidator()
+    {
+        RuleFor(r => r.Name).Length(5).WithMessage("Please enter a name repository");
+    }
+    
+}
