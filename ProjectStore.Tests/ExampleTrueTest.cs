a98e38630f1e01b564018bbbb9706cd9f8f008bc
artemov <artik3314@gmail.com>
2024-08-02 13:25:05 +0300

    Проверка тестирования

---
+++ ProjectStore.Tests/ExampleTrueTest.cs
@@ -0,0 +1,12 @@
+using System.Diagnostics;
+
+namespace ProjectStore.Tests;
+
+public class ExampleTrueTest
+{
+    [Fact]
+    public void SuccessTest()
+    {
+        Assert.Equal(1,1);
+    }
+}
